5afcd2666d719b0398d66e3c22532c6b559bf1db
plalfonzo <plalfonzo4hotmail.com>
2025-05-21 11:26:19 -0300

    commit se agrego la carpeta contendio

---
+++ Contenido/contenido_contacto.php
@@ -0,0 +1,22 @@
+<h1 class="text-center">Contacto</h1>
+  
+<section class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <h1>Datos de contacto</h1>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur assumenda eaque quibusdam. Ducimus, rerum, sequi pariatur eveniet eos neque, minima explicabo quos assumenda saepe consectetur optio culpa officiis in! Vel.
+        </div>
+
+        <div class="col-md-8">
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d28321.64404636926!2d-58.8382208!3d-27.4628608!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1747836177038!5m2!1ses!2sar" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+        </div>
+    </div>
+</section>
+
+
+
+
+
---
+++ Contenido/contenido_index.php
@@ -0,0 +1,70 @@
+
+
+  <section class="mt-3 container" id="productos">
+    <h1 class="text-center display-4 animate__animated animate__fadeInLeft">Nuestros productos</h1>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card h-100">
+          <img src="./assets/img/img2.jpg" class="card-img-top img-car" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+              content. This content is a little bit longer.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="./assets/img/img3.jpeg" class="card-img-top img-car" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="./assets/img/mi-foto.jpeg" class="card-img-top img-car" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+              content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+  <section class="mt-5 container ">
+    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="./assets/img/img1.jpg" class="d-block w-100" style="max-height:50vh;" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="./assets/img/img2.jpg" class="d-block w-100" style="max-height:50vh;" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="./assets/img/img3.jpeg" class="d-block w-100" style="max-height:50vh;" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </section>
